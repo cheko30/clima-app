110f33af6200e6bdcb321235a50534986688de1a
sergio.pena <sergio.pena.orozco@gmail.com>
2024-11-04 22:03:46 -0600

    add form component with styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import styles from "./App.module.css"
+import Form from "./components/form/Form"
 
 function App() {
   return (
     <>
       <h1 className={styles.title}>Buscador Clima</h1>
+      <div className={styles.container}>
+        <Form />
+
+      </div>
     </>
   )
 }
