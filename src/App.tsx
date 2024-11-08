b4ef84a82f45d5cf8e62ec814bc16f29c31a9429
sergio.pena <sergio.pena.orozco@gmail.com>
2024-11-07 21:40:25 -0600

    add spinner when call api

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import styles from "./App.module.css"
+import Alert from "./components/Alert/Alert"
 import Form from "./components/form/Form"
+import Spinner from "./components/Spinner/Spinner"
+import WeatherDetail from "./components/WeatherDetail/WeatherDetail"
 import useWeather from "./hooks/useWeather"
 
 function App() {
 
-  const { fechtWeather } = useWeather()
+  const { fechtWeather, loading, notFound, weather, hasWeatherData } = useWeather()
   return (
     <>
       <h1 className={styles.title}>Buscador Clima</h1>
@@ -12,7 +15,9 @@ function App() {
         <Form 
           fechtWeather={fechtWeather}
         />
-
+        {loading && <Spinner />}
+        {hasWeatherData && <WeatherDetail weather={weather} />}
+        {notFound && <Alert>Ciudad No Encontrada</Alert>}
       </div>
     </>
   )
--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,44 +1,75 @@
 import axios from "axios"
-import { object, string, number, InferOutput, parse } from "valibot"
+import { z } from "zod"
 import { SearchType } from "../types"
+import { useMemo, useState } from "react"
+
+//Zod
+const Weather = z.object({
+    name: z.string(),
+    main: z.object({
+        temp: z.number(),
+        temp_max: z.number(),
+        temp_min: z.number(),
+    })
+})
+
+export type Weather = z.infer<typeof Weather>
+
+const initalState = {
+    name: "",
+    main: {
+        temp: 0,
+        temp_max: 0,
+        temp_min: 0,
+    }
+}
 
 export default function useWeather() {
-
-    //Valibot
-    const WeatherSchema = object({
-        name: string(),
-        main: object({
-            temp: number(),
-            temp_max: number(),
-            temp_min: number(),
-        })
-    })
-
-    type Weather = InferOutput<typeof WeatherSchema>
+    
+    const [weather, setWeather] = useState<Weather>(initalState)
+    const [loading, setLoading] = useState(false)
+    const [notFound, setNotFound] = useState(false)
 
     const fechtWeather =  async (search: SearchType) => {
+        setLoading(true)
+        setWeather(initalState)
         try {
             const appId = import.meta.env.VITE_API_KEY
             const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
 
             const {data} = await axios(geoUrl)
+
+            if(!data[0]) {
+                setNotFound(true)
+                return
+            }
             
             const lat = data[0].lat
             const lon = data[0].lon
 
             const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`
 
-            //Valibot
+            //Zod
             const {data: weatherResult} = await axios(weatherUrl)
-            const result = parse(WeatherSchema, weatherResult)
-            console.log(result)            
+            const result = Weather.safeParse(weatherResult)
+            if(result.success) {
+                setWeather(result.data)
+            }        
             
         } catch (error) {
             console.log(error)
+        } finally {
+            setLoading(false)
         }
     }
 
+    const hasWeatherData = useMemo(() => weather.name, [weather])
+
     return {
-        fechtWeather
+        weather,
+        loading,
+        notFound,
+        fechtWeather,
+        hasWeatherData,
     }
 }
